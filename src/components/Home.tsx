72901f5a3d8122ae96572cf478438d4abc4a04ad
Kento-Ishizaki <kentoron1115@gmail.com>
2023-11-09 21:17:06 +0800

    feat: smooth scrolling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,10 @@
 import HeroImage from '../assets/heroImage.png';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div data-name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div id="home" data-name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Software Developer</h2>
@@ -12,12 +13,17 @@ const Home = () => {
             現在はセブ島に住んでおり、日本とセブ島の2つの企業で働いています。
           </p>
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-violet-500 to-cyan-500">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-violet-500 to-cyan-500"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
